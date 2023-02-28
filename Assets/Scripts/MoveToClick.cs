5e66b479f71c03963bbe45d30971a11cf7feeeaf
hunte <besscomdark@gmail.com>
2023-02-28 17:14:34 +0800

    腳色移動控制和新增遊戲階梯

--- Assets/Scripts/MoveToClick.cs
+++ Assets/Scripts/MoveToClick.cs
@@ -22,7 +22,19 @@ namespace henry
         // Update is called once per frame
         void Update()
         {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Camera cameraConponent = GameObject.Find("Main Camera").GetComponent<Camera>();
+                Ray ray = cameraConponent.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
 
+                // Physics.Raycast 它會射出一條虛擬射線(穿越場景)，並告訴你它有沒有射到任何東西。
+
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    agent.SetDestination(hit.point);
+                }
+            }
         }
     }
 }
